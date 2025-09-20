20a9fcf84dd96310275d2a3ec2727010fca294cf
Kpstya <kv.dryakhlov@bgit.ru>
2025-09-21 00:25:05 +0300

    delete example test and route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,12 +6,10 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         api: [
             __DIR__.'/../routes/api/v1/api.php',
             __DIR__.'/../routes/api/v2/api.php',
         ],
-        commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
