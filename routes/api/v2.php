52b139dc4118d0aa1a914f13f4b08a05023cc390
Kpstya <kv.dryakhlov@bgit.ru>
2026-01-26 11:24:57 +0300

    add test for product v1 endpoint

--- routes/api/v2.php
+++ routes/api/v2.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\v2\UserController;
 use Illuminate\Support\Facades\Route;
 
 /* TODO kpstya
-    - стоит ли избавиться от этого эндпоинта storage/{path}
+    - стоит ли избавиться от эндпоинта storage/{path}
     - как это сделано в наших рабочих проектах? */
 
 Route::prefix('v2')->name('api.v2.')->group(function (): void {
